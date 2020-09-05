53dd7a7d71560ba8f5d90c6d136dab0c1e5ed4a6
Patrik Drab <patrikdrab75@gmail.com>
2020-09-05 13:40:41 +0200

    add AddData

--- src/routes/Dashboard/Dashboard.js
+++ src/routes/Dashboard/Dashboard.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import NavBar from '../../components/NavBar/NavBar';
 import classes from './Dashboard.module.scss';
+import AddData from './AddData';
 
 const Dashboard = ({ items, children }) => {
     return (
         <div>
             <NavBar title="pacient" items={items} />
-            <div className={classes.Content}>{children}</div>
+            <div className={classes.Content}>
+                {children}
+                <AddData/>
+            </div>
         </div>
     );
 };
