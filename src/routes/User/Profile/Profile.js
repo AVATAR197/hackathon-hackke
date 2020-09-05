4b3656c4061375514ed4cba0d5003f41a9b6eaa9
Patrik Drab <patrikdrab75@gmail.com>
2020-09-05 19:51:20 +0200

    next update styles

--- src/routes/User/Profile/Profile.js
+++ src/routes/User/Profile/Profile.js
@@ -4,7 +4,7 @@ import classes from './Profile.module.scss';
 const Profile = () => {
     return (
         <div className={classes.Wrapper}>
-            <h1>Profile</h1>
+            <div className={classes.Profile}>
             <div className={classes.Image}>
                 <img
                     src="https://images.pexels.com/photos/1681010/pexels-photo-1681010.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
@@ -14,18 +14,21 @@ const Profile = () => {
 
             <table>
                 <tbody>
+                    
+                    <tr>
+                        <td className={classes.Info}>Name:</td>
+                        <td className={classes.Data}>Jack John</td>
+                    </tr>
+                    
                     <tr>
                         <td className={classes.Info}>Diabetes type:</td>
                         <td className={classes.Data}>2</td>
                     </tr>
 
-                    <tr>
-                        <td className={classes.Info}>Name:</td>
-                        <td className={classes.Data}>Jack John</td>
-                    </tr>
+                    
 
                     <tr>
-                        <td className={classes.Info}>BirthDate:</td>
+                        <td className={classes.Info}>Birth date:</td>
                         <td className={classes.Data}>20.5.2001</td>
                     </tr>
 
@@ -35,6 +38,8 @@ const Profile = () => {
                     </tr>
                 </tbody>
             </table>
+            </div>
+            
         </div>
     );
 };
