deef33f67a9ec8f2207580946e7bfc48f8d6c2ed
Patrik Drab <patrikdrab75@gmail.com>
2020-09-05 15:31:53 +0200

    update

---
+++ src/routes/User/MainPage/DataTable.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import {useTable} from "react-table";
+
+
+function DataTable() {
+    return (
+        <table>
+            
+        </table>
+    )
+}
+
+export default DataTable
--- src/routes/User/MainPage/MainPage.js
+++ src/routes/User/MainPage/MainPage.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import AddDataButton from '../../../components/AddDataButton/AddDataButton';
 import classes from './MainPage.module.scss'
+import DataTable from './DataTable';
 
 const Dashboard = () => {
+    
     return (
         <div className={classes.cc}>
             <h1>Main Page</h1>
+            <DataTable/>
             <AddDataButton/>
         </div>
     );
